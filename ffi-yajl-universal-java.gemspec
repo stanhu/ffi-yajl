stanhu/ffi-yajl

commit 78b18e4a454b0b669aca0a26ce79259cb1365582

Updating for Ruby 3.4 and Cookstyle 8.1 support (#128)
* Updating for Ruby 3.4 and Cookstyle 8.1 support
Signed-off-by: John McCrae <john.mccrae@progress.com>

ffi-yajl-universal-java.gemspec
@@ -1,11 +1,0 @@
-gemspec = eval(IO.read(File.expand_path(File.join(File.dirname(__FILE__), "ffi-yajl.gemspec.shared"))))
-
-gemspec.platform = "universal-java"
-
-# XXX: after the libyajl2-gem, we don't need to fork the
-# extensions so can we simplify the gemspecs now?
-# gemspec.extensions = %w{ ext/libyajl2/extconf.rb }
-
-gemspec.add_runtime_dependency "ffi", "~> 1.5"
-
-gemspec

ffi-yajl.gemspec.shared
@@ -1,24 +1,0 @@
-$:.unshift(File.dirname(__FILE__) + '/lib')
-require 'ffi_yajl/version'
-
-Gem::Specification.new do |s|
-  s.name             = 'ffi-yajl'
-  s.version          = FFI_Yajl::VERSION
-  s.extra_rdoc_files = ["README.md", "LICENSE" ]
-  s.license          = "MIT"
-  s.summary          = "Ruby FFI wrapper around YAJL 2.x"
-  s.description      = s.summary
-  s.author           = "Lamont Granquist"
-  s.email            = "lamont@chef.io"
-  s.homepage         = "http://github.com/chef/ffi-yajl"
-
-  s.required_ruby_version = ">= 2.2"
-
-  s.add_dependency "libyajl2", ">= 1.2"
-
-  s.bindir       = "bin"
-  s.executables  = %w{ ffi-yajl-bench }
-  s.require_path = 'lib'
-  s.files        = %w{ LICENSE } +
-    Dir.glob( "{bin,lib,ext}/**/*", File::FNM_DOTMATCH ).reject { |f| File.directory?(f) }
-end

.expeditor/buildkite/run_windows_tests.ps1
@@ -6,18 +6,15 @@ param([String]$version)
 # unneccesary to check the exit code of each program being run - non-zero exit will force it to fail and terminate.
 $ErrorActionPreference = "Stop"
 
-# The specific paths of tools within the ruby30/31 devkit vary a bit across 3.0 and 3.1
-if ($version -eq "3.0")
-{
-    $base_dir = "C:\ruby30\"
-    $Env:PATH += ";" + $base_dir + "ruby\bin;" + $base_dir + "msys64\usr\bin;" + $base_dir + "msys64\mingw64\bin"
-}
-elseif($version -eq "3.1")
-{
-    $base_dir = "C:\ruby31\"
-    # Note path change - gcc is living in ucrt64\bin here, and mingw64 in earlier versions.
-    $Env:PATH += ";" + $base_dir + "ruby\bin;" + $base_dir + "msys64\usr\bin;" + $base_dir + "msys64\ucrt64\bin"
-}
+Write-Output "--- Updating system gems"
+gem update --system
+
+Write-Output "--- Ensuring Make and GCC are installed"
+$gccs = Get-ChildItem -path c:\ gcc.exe -Recurse -ErrorAction SilentlyContinue
+$env:path = "$($gccs[0].DirectoryName)" + ";" + $env:path 
+
+$makes = Get-ChildItem -Path c:\ make.exe -Recurse -ErrorAction SilentlyContinue
+$env:path = "$($makes[0].DirectoryName)" + ";" + $env:path
 
 Write-Output "--- Ensuring required bins are in path"
 Write-Output  "PATH: " + $Env:PATH
@@ -25,9 +22,6 @@ make --version
 gcc --version
 ruby --version
 bundler --version
-
-Write-Output "--- Updating system gems"
-gem update --system
 
 Write-Output "--- Bundle install"
 bundle install --without development_extras --jobs 3 --retry 3 --path vendor/bundle

.expeditor/buildkite/verify.ps1
@@ -1,19 +1,21 @@
-echo "--- system details"
+Write-Output "--- system details"
 $Properties = 'Caption', 'CSName', 'Version', 'BuildType', 'OSArchitecture'
 Get-CimInstance Win32_OperatingSystem | Select-Object $Properties | Format-Table -AutoSize
 
-echo "--- Install make"
+Write-Output "--- Install make"
 choco install make -y
 refreshenv
 If ($lastexitcode -ne 0) { Exit $lastexitcode }
 
-choco install msys2 -y
-refreshenv
+Write-Output "--- Installing DevKit2"
+$installDir = "C:\msys_tools"
+choco install msys2 --params "/NoUpdate /InstallDir:$installDir" -y
 If ($lastexitcode -ne 0) { Exit $lastexitcode }
 
-Write-Output 'Updating PATH'
-$env:PATH = "C:\tools\ruby26\bin;C:\tools\DevKit2\mingw\bin;C:\tools\DevKit2\bin;" + $env:PATH
+Write-Output "--- Updating PATH"
+$env:PATH = "$installDir\bin;C:\tools\DevKit2\mingw\bin;C:\tools\DevKit2\bin;" + $env:PATH
 [Environment]::SetEnvironmentVariable('PATH', $env:PATH, [EnvironmentVariableTarget]::Machine)
+refreshenv
 If ($lastexitcode -ne 0) { Exit $lastexitcode }
 
 ruby -v
@@ -21,18 +23,18 @@ bundle --version
 gem -v
 If ($lastexitcode -ne 0) { Exit $lastexitcode }
 
-echo "--- gem install bundler"
+Write-Output "--- gem install bundler"
 gem install bundler
 If ($lastexitcode -ne 0) { Exit $lastexitcode }
 
-echo "--- bundle install"
+Write-Output "--- bundle install"
 bundle install --without development_extras --jobs 3 --retry 3 --path vendor/bundle
 If ($lastexitcode -ne 0) { Exit $lastexitcode }
 
-echo "+++ bundle exec rake compile"
+Write-Output "+++ bundle exec rake compile"
 bundle exec rake compile
 If ($lastexitcode -ne 0) { Exit $lastexitcode }
 
-echo "+++ bundle exec rake spec"
+Write-Output "+++ bundle exec rake spec"
 bundle exec rake spec
 If ($lastexitcode -ne 0) { Exit $lastexitcode }

.expeditor/config.yml
@@ -7,7 +7,7 @@ slack:
 # This publish is triggered by the `built_in:publish_rubygems` artifact_action.
 rubygems:
   - ffi-yajl
-  - ffi-yajl-universal-java
+  - ffi-yajl-universal-mingw-ucrt
 
 github:
   # This deletes the GitHub PR branch after successfully merged into the release branch

.expeditor/verify.pipeline.yml
@@ -1,27 +1,5 @@
 steps:
 
-# Allow ruby-2.4 to softfail since it was configured so in travis too
-- label: ":ruby: 2.6"
-  command:
-    - .expeditor/buildkite/run_linux_tests.sh
-  expeditor:
-    executor:
-      docker:
-        image: ruby:2.6
-        environment:
-          - BUNDLE_GEMFILE=/workdir/Gemfile
-          - FORCE_FFI_YAJL="ext"
-
-- label: ":ruby: 3.0"
-  command:
-    - .expeditor/buildkite/run_linux_tests.sh
-  expeditor:
-    executor:
-      docker:
-        image: ruby:3.0
-        environment:
-          - BUNDLE_GEMFILE=/workdir/Gemfile
-          - FORCE_FFI_YAJL="ext"
 
 - label: ":ruby: 3.1"
   command:
@@ -30,21 +8,17 @@ steps:
     executor:
       docker:
         image: ruby:3.1
-        privileged: true
         environment:
           - BUNDLE_GEMFILE=/workdir/Gemfile
           - FORCE_FFI_YAJL="ext"
 
-- label: ":windows: :ruby: 3.0"
+- label: ":ruby: 3.4"
   command:
-    - .expeditor/buildkite/run_windows_tests.ps1 "3.0"
-  timeout_in_minutes: 20
+    - .expeditor/buildkite/run_linux_tests.sh
   expeditor:
     executor:
       docker:
-        host_os: windows
-        shell: ["powershell", "-Command"]
-        image: rubydistros/windows-2019:3.0
+        image: ruby:3.4
         environment:
           - BUNDLE_GEMFILE=/workdir/Gemfile
           - FORCE_FFI_YAJL="ext"
@@ -59,6 +33,20 @@ steps:
         host_os: windows
         shell: ["powershell", "-Command"]
         image: rubydistros/windows-2019:3.1
+        environment:
+          - BUNDLE_GEMFILE=/workdir/Gemfile
+          - FORCE_FFI_YAJL="ext"
+
+- label: ":windows: :ruby: 3.4"
+  command:
+    - .expeditor/buildkite/run_windows_tests.ps1 "3.4"
+  timeout_in_minutes: 20
+  expeditor:
+    executor:
+      docker:
+        host_os: windows
+        shell: ["powershell", "-Command"]
+        image: rubydistros/windows-2019:3.4
         environment:
           - BUNDLE_GEMFILE=/workdir/Gemfile
           - FORCE_FFI_YAJL="ext"

.github/workflows/lint.yml
@@ -1,0 +1,27 @@
+---
+name: lint
+
+on:
+  pull_request:
+  push:
+    branches:
+      - main
+
+concurrency:
+  group: lint-${{ github.ref }}
+  cancel-in-progress: true
+
+jobs:
+  lint:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v4
+      - uses: ruby/setup-ruby@v1
+        with:
+          ruby-version: 3.1
+          bundler-cache: false
+      - uses: r7kamura/rubocop-problem-matchers-action@v1  # this shows the failures in the PR
+      - run: |
+          gem install cookstyle
+          cookstyle --chefstyle -c .rubocop.yml
+  

.github/workflows/unit-test.yml
@@ -1,0 +1,31 @@
+name: Unit Test
+on:
+  - push
+  - pull_request
+jobs:
+  test:
+    runs-on: ${{ matrix.os }}
+    continue-on-error: ${{ matrix.experimental }}
+    strategy:
+      fail-fast: false
+      matrix:
+        ruby: [ '3.1' , '3.4' ]
+        os:
+          - windows-latest
+        experimental: [false]
+    name: Unit testing with Ruby ${{ matrix.ruby }} on ${{ matrix.os }}
+    steps:
+    - uses: actions/checkout@v2
+    - uses: ruby/setup-ruby@v1
+      with:
+        ruby-version: ${{ matrix.ruby }}
+    - name: Install
+      run: |
+        ruby --version
+        gem --version
+        gem install bundler rake
+        bundle install --jobs 4 --retry 3
+    - name: Unit Test
+      run: |
+        rake compile
+        bundle exec rake spec

commit 373a89c35c7052721a0be40e770eea5d421775c5

Fix 2.7.x tags not building extensions
78b18e4a454b0b669aca0a26ce79259cb1365582 broke building of 2.7.

ffi-yajl-universal-java.gemspec
@@ -1,0 +1,11 @@
+gemspec = eval(IO.read(File.expand_path(File.join(File.dirname(__FILE__), "ffi-yajl.gemspec.shared"))))
+
+gemspec.platform = "universal-java"
+
+# XXX: after the libyajl2-gem, we don't need to fork the
+# extensions so can we simplify the gemspecs now?
+# gemspec.extensions = %w{ ext/libyajl2/extconf.rb }
+
+gemspec.add_runtime_dependency "ffi", "~> 1.5"
+
+gemspec

ffi-yajl.gemspec
@@ -1,29 +1,6 @@
-# frozen_string_literal: true
-$:.unshift File.expand_path("lib", __dir__)
-require "ffi_yajl/version"
+gemspec = eval(IO.read(File.expand_path(File.join(File.dirname(__FILE__), "ffi-yajl.gemspec.shared"))))
 
-Gem::Specification.new do |s|
-  s.name             = "ffi-yajl"
-  s.version          = FFI_Yajl::VERSION
-  s.extra_rdoc_files = ["README.md", "LICENSE" ]
-  s.license          = "MIT"
-  s.summary          = "Ruby FFI wrapper around YAJL 2.x"
-  s.description      = s.summary
-  s.author           = "Lamont Granquist"
-  s.email            = "lamont@chef.io"
-  s.homepage         = "http://github.com/chef/ffi-yajl"
+gemspec.platform = Gem::Platform::RUBY
+gemspec.extensions = %w{ext/ffi_yajl/ext/encoder/extconf.rb ext/ffi_yajl/ext/parser/extconf.rb ext/ffi_yajl/ext/dlopen/extconf.rb}
 
-  s.required_ruby_version = ">= 3.1"
-
-  s.add_dependency "libyajl2", ">= 2.1"
-
-  s.bindir       = "bin"
-  s.executables  = %w{ ffi-yajl-bench }
-  s.require_path = "lib"
-  s.files        = %w{ LICENSE } + Dir.glob( "{bin,lib,ext}/**/*", File::FNM_DOTMATCH ).reject { |f| File.directory?(f) }
-
-  s.add_dependency "yajl"
-
-  s.add_development_dependency "cookstyle", "~> 8.1"
-  s.platform = Gem::Platform::RUBY
-end
+gemspec

ffi-yajl.gemspec.shared
@@ -1,0 +1,24 @@
+$:.unshift(File.dirname(__FILE__) + '/lib')
+require 'ffi_yajl/version'
+
+Gem::Specification.new do |s|
+  s.name             = 'ffi-yajl'
+  s.version          = FFI_Yajl::VERSION
+  s.extra_rdoc_files = ["README.md", "LICENSE" ]
+  s.license          = "MIT"
+  s.summary          = "Ruby FFI wrapper around YAJL 2.x"
+  s.description      = s.summary
+  s.author           = "Lamont Granquist"
+  s.email            = "lamont@chef.io"
+  s.homepage         = "http://github.com/chef/ffi-yajl"
+
+  s.required_ruby_version = ">= 2.2"
+
+  s.add_dependency "libyajl2", ">= 1.2"
+
+  s.bindir       = "bin"
+  s.executables  = %w{ ffi-yajl-bench }
+  s.require_path = 'lib'
+  s.files        = %w{ LICENSE } +
+    Dir.glob( "{bin,lib,ext}/**/*", File::FNM_DOTMATCH ).reject { |f| File.directory?(f) }
+end